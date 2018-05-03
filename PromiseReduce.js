5d9c97c177878ad781ff70104416cbc326e38884
VukZh <36912856+VukZh@users.noreply.github.com>
2018-05-03 12:47:29 +0600

    new version

--- PromiseReduce.js
+++ PromiseReduce.js
@@ -1,35 +1,49 @@
- async function PromiseReduce() {
+async function PromiseReduce() {
 
-     var arrayResolve = [];
-     //var flag = 1;
-     var reduceFun = arguments[1];
-     var reduceInitValue = arguments[2];
-     var res;
+  var arrayResolve = [];
+  var flag = 1;
+  var reduceFun = arguments[1];
+  var reduceInitValue = arguments[2];
+  var res = 0;
+  var i = 0;
 
-     var TempArray = function(x) {
-         x.forEach((r) => (r.then((res) => (arrayResolve.push(res))).catch(() => (flag = 0))))///<--
-     }
-     await TempArray(arguments[0]);
+  var TempArray = async function(x) {
+    while (i < x.length) {
+      await x[i].then((val) => {
+        arrayResolve.push(val);
+        console.log('-', val);
+      }).catch(() => {
+        flag = 0;
+      });
+      i++;
+    }
+  }
 
-     //console.log("flag-",flag)
-     //setTimeout (()=>(console.log("flag-",flag)),0)
+  await TempArray(arguments[0]);
 
-     if (arrayResolve.length != arguments[0].length) throw new Error('Oops'); ////// <--
+  if (flag === 0)
+    throw new Error('Oops');
 
-     if (typeof(reduceInitValue) != 'undefined') {
-         res = arrayResolve.reduce(reduceFun, reduceInitValue);
-     } else {
-         res = arrayResolve.reduce(reduceFun);
-     };
+  if (typeof(reduceInitValue) != 'undefined') {
+    res = arrayResolve.reduce(reduceFun, reduceInitValue);
+  } else {
+    res = arrayResolve.reduce(reduceFun);
+  };
 
-     return res;
+  return (res);
 
- }
+}
 
- // var p1 = Promise.resolve(13);
- // var p2 = Promise.resolve(2);
- // var p3 = Promise.resolve(9);
- // var p4 = Promise.resolve(1);
- // var p5 = Promise.resolve(3);
- // PromiseReduce([p1,p2,p3,p4],(a,b)=>(a*b)).then ((r)=>(console.log(r)))
- // PromiseReduce([p1,p2,p3,p5],(a,b)=>(a*b)).then ((r)=>(console.log(r)))
+// var p1 = Promise.resolve(13);
+// var p2 = Promise.resolve(2);
+// var p3 = Promise.resolve(9);
+// var p4 = Promise.resolve(1);
+// var p5 = Promise.reject(3);
+// var p6 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve(100);
+//   }, 5000);
+// });
+// PromiseReduce([p1,p2,p3,p4],(a,b)=>(a*b)).then ((r)=>(console.log(r)))
+// PromiseReduce([p1,p2,p3,p5],(a,b)=>(a*b)).then ((r)=>(console.log(r)))
+// PromiseReduce([p1,p2,p3,p6],(a,b)=>(a*b)).then ((r)=>(console.log(r)))
